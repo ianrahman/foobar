7cd4be698225165222198b2aaa1ded02980c35f1
Mark Holland <mark@partiallogic.com>
2014-12-03 01:16:52 +0100

    Level 2 complete

--- Access Codes/accessCodes.py
+++ Access Codes/accessCodes.py
@@ -5,9 +5,10 @@ def answer(x):
 		if l != "".join(reversed(l)):
 			check = "".join(reversed(l))
 			if check in x:
+				distinct = distinct - 1
 				x.remove(check)
 				x.remove(l)
-				distinct = distinct - 1
+				
 	return distinct
 
 
